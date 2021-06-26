8637e60c543afdd7dad0dbb9d5bc38c62cacce3e
Sorrybabe <86021848+Sorrybabe@users.noreply.github.com>
2021-06-26 19:28:34 +0000

    Create Yukki.py

---
+++ Yukki.py
@@ -0,0 +1,99 @@
+print("[INFO]: INITIALIZING")
+from pyrogram import Client
+import asyncio
+from .config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, LOG_ID
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+import time
+import uvloop
+import logging
+import os
+import psutil
+import time
+from datetime import datetime
+from pyrogram import Client as Bot
+from .config import API_ID, API_HASH, BOT_TOKEN, MONGO_DB_URI, SUDO_USERS, LOG_GROUP_ID
+from pyrogram import Client, idle
+from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
+import time
+
+print("[INFO]: INITIALIZING DATABASE")
+MONGODB_CLI = MongoClient(MONGO_DB_URI)
+db = MONGODB_CLI.wbb
+
+
+YUKKI_START_TIME = time.time()
+
+
+
+print("[INFO]: INITIALIZING BOT CLIENT")
+app = Client(
+    'YukkiBot',
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+)
+
+print("[INFO]: STARTING BOT CLIENT")
+app.start()
+print("[INFO]: STARTED")
+
+
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - YUKKI_START_TIME)
+    cpu = psutil.cpu_percent(interval=0.5)
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    stats = f'''
+Uptime: {get_readable_time((bot_uptime))}'''
+    return stats
+
+
+@app.on_message(filters.command(["ping", "start", "alive"]) & filters.user(SUDOERS))
+async def ping(_, message):
+    start = datetime.now()
+    uptime = await bot_sys_stats()
+    end = datetime.now()
+    resp = (end - start).microseconds / 1000
+    await message.reply_text(f"**Alive!**\n**Responding in**`⚡{resp} ms`\n\n<b><u>Uptime:</u></b>{uptime}")   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+idle()
