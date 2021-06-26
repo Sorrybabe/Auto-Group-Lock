44be4aaf8a644926fd3b9ba994fc303e3a824772
Sorrybabe <86021848+Sorrybabe@users.noreply.github.com>
2021-06-26 19:29:10 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = getenv('BOT_TOKEN')
+API_ID = int(getenv('API_ID', ''))
+API_HASH = getenv('API_HASH')
+COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ !').split())
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+SUDO_USERS = list(map(int, getenv('SUDO_USERS', '').split()))
+LOGID = int(getenv("LOG_GROUP_ID", ''))
+ASS_ID = int(getenv("ASS_ID", ''))
